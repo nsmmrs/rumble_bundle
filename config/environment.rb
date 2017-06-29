23f0b9c68bf0518433e772bd77cef8196ae2f9dc
Noah Summers <summers.noah@gmail.com>
2017-06-29 14:31:46 -0400

    add lib requirements

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,8 @@ require "bundler/setup"
 require "nokogiri"
 
 require "rumble_bundle"
+
+require_relative "../lib/rumble_bundle/cli"
+require_relative "../lib/rumble_bundle/scraper"
+require_relative "../lib/rumble_bundle/bundle"
+require_relative "../lib/rumble_bundle/product"
