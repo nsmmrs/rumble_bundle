95ecedd3dcf2435906b0566b97179fd3454d06cf
Noah Summers <summers.noah@gmail.com>
2017-06-29 18:06:31 -0400

    add missing self.all methods for Product

--- lib/rumble_bundle/product.rb
+++ lib/rumble_bundle/product.rb
@@ -6,6 +6,14 @@ class RumbleBundle::Product
 
   @@all = []
 
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    self.all.clear
+  end
+
   def initialize
     self.class.all << self
   end
