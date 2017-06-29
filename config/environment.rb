03e5dc8de1eb58e13bcaceaf67b682c39a11b0df
Noah Summers <summers.noah@gmail.com>
2017-06-29 15:29:46 -0400

    format environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,10 @@
-require "bundler/setup"
-require "nokogiri"
+require 'bundler/setup'
+require 'open-uri'
+require 'nokogiri'
 
-require "rumble_bundle"
+require 'rumble_bundle'
 
-require_relative "../lib/rumble_bundle/cli"
-require_relative "../lib/rumble_bundle/scraper"
-require_relative "../lib/rumble_bundle/bundle"
-require_relative "../lib/rumble_bundle/product"
+require_relative '../lib/rumble_bundle/cli'
+require_relative '../lib/rumble_bundle/scraper'
+require_relative '../lib/rumble_bundle/bundle'
+require_relative '../lib/rumble_bundle/product'
