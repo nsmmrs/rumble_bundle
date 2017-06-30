bf75848dac05ddf22dbd9bc20f492bea788ba8ce
Noah Summers <summers.noah@gmail.com>
2017-06-29 23:09:38 -0400

    start CLI::start and ::query methods

--- lib/rumble_bundle/cli.rb
+++ lib/rumble_bundle/cli.rb
@@ -1,11 +1,31 @@
-# The CLI class will be responsible for calling the scraper on startup, and all user interaction.
-
 class RumbleBundle::CLI
-  def initialize
-    
-  end
+  # def initialize
+  # end
 
   def self.start
-    
+    puts "Fetching data from HumbleBundle.com..."
+
+    RumbleBundle::Scraper.new.crawl_site
+
+    query
+
   end
+
+  def self.query
+    puts "Enter a bundle's number to learn more, or enter 'help' for more commands."
+    puts "Enter 'quit' to leave the program."
+
+    RumbleBundle::Bundle.all.each.with_index(1) do |bundle, i|
+      puts "[#{i}] #{bundle.name}"
+    end
+  end
+
+  def self.display(bundle)
+  end
+
+  def self.help
+  end
+
+
 end
+
