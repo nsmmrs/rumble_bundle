189c1d4df0cb816d58b6035b7944559f26433738
Noah Summers <summers.noah@gmail.com>
2017-06-29 23:40:44 -0400

    add CLI::help and ::quit methods

--- lib/rumble_bundle/cli.rb
+++ lib/rumble_bundle/cli.rb
@@ -8,17 +8,21 @@ class RumbleBundle::CLI
     puts ""
 
     RumbleBundle::Scraper.new.crawl_site
+    bundles
 
     query
 
+  end
+
+  def self.bundles
+    @@bundles ||= RumbleBundle::Bundle.all
   end
 
   def self.query
     puts ""
     puts "Enter a bundle's number to learn more, or enter 'help' for more commands."
     puts "Enter 'quit' to leave the program."
-
-    bundles = RumbleBundle::Bundle.all
+    puts ""
 
     bundles.each.with_index(1) do |bundle, i|
       puts "[#{i}] #{bundle.name}"
@@ -26,14 +30,13 @@ class RumbleBundle::CLI
 
     input = gets.strip
     if input.to_i.between?(1, bundles.length)
-      binding.pry
       display(bundles[input.to_i - 1])
-    # else
-    #   case input
-    #   when 'help'
-    #     help
-    #   when 'quit'
-    #     quit
+    else
+      case input
+      when 'help'
+        help
+      when 'quit'
+        quit
       # when 'help'
       # when 'drm-free'
       # when 'steam-key'
@@ -41,7 +44,7 @@ class RumbleBundle::CLI
       # when 'mac'
       # when 'linux'
       # when 'android'
-      # end
+      end
 
 
     end
@@ -54,9 +57,29 @@ class RumbleBundle::CLI
   end
 
   def self.help
+    puts <<~HEREDOC
+    ---------------------------------------------------------------
+    You can enter a filter to list all products matching that filter.
+    You can combine multiple filters in a space-separated list.
+
+      e.g. 'drm-free linux'
+
+    drm-free
+    steam-key
+    windows
+    mac
+    linux
+    android
+
+    Press any key to continue.
+    ---------------------------------------------------------------
+    HEREDOC
+    STDIN.getch
+    query
   end
 
   def self.quit
+    puts "I'm quitting!"
   end
 
   def self.filter(arguments)
