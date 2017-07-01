730fa89362052b9fa98c7e252e5c548b5e4fdbd9
Noah Summers <summers.noah@gmail.com>
2017-07-01 05:19:02 -0400

    correct environment load order

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ require 'nokogiri'
 
 require 'rumble_bundle'
 
-require_relative '../lib/rumble_bundle/cli'
-require_relative '../lib/rumble_bundle/scraper'
 require_relative '../lib/rumble_bundle/bundle'
-require_relative '../lib/rumble_bundle/product'
 require_relative '../lib/rumble_bundle/tier'
+require_relative '../lib/rumble_bundle/product'
+require_relative '../lib/rumble_bundle/scraper'
+require_relative '../lib/rumble_bundle/cli'
